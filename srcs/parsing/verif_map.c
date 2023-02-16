349c32127dd049f2daa46f687dbb952d6a743831
0x35c <unai64@orange.fr>
2023-02-16 14:18:46 +0100

    check_map started

--- srcs/parsing/verif_map.c
+++ srcs/parsing/verif_map.c
@@ -12,9 +12,48 @@
 
 #include "../../includes/parsing.h"
 
-void	verif_map(t_map *map, t_texture *config)
+static int	max_line_length(char **map)
+{
+	int	i;
+	int	j;
+	int	length;
+
+	length = 0;
+	i = 0;
+	while (map[i] != NULL)
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			j++;
+		}
+		if (j > length)
+			length = j;
+		i++;
+	}
+	return (length);
+}
+
+static void	check_line_format(char *line, int max_length)
 {
 	int	i;
 
 	i = 0;
+	while (line[i] && ft_isspace(line[i]))
+	{
+	}
+}
+
+void	verif_map(t_texture *config)
+{
+	const int	max_length = max_line_length(config->map);
+	const char	**map = config->map;
+	int			i;
+	int			j;
+
+	i = 0;
+	while (map[i] != NULL)
+	{
+		check_line_format(map[i], max_length);
+	}
 }
