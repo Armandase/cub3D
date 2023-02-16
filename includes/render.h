c71ce71a519829912b4ca0666b5ad83aee4f9d06
Armandase <adamiens@student.42angouleme.fr>
2023-02-16 13:57:33 +0100

    initialisation of render

--- includes/render.h
+++ includes/render.h
@@ -6,7 +6,7 @@
 /*   By: adamiens <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 09:39:34 by adamiens          #+#    #+#             */
-/*   Updated: 2023/02/16 09:40:51 by adamiens         ###   ########.fr       */
+/*   Updated: 2023/02/16 13:53:44 by adamiens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,23 @@
 # define RENDER_H
 
 # include "parsing.h"
+# include "../libs/mlx/mlx.h"
+
+# define WIDTH 1080
+# define HEIGHT 720
+
+typedef struct s_mlx
+{
+	void	*init;
+	void	*win;
+	void	*img;
+	char	*img_addr;
+	int		img_width;
+	int		img_height;
+	int		bits_per_px;
+	int		line_len;
+	int		endian;
+}	t_mlx;
 
 void	render(t_texture *config);
 
--- srcs/render/render.c
+++ srcs/render/render.c
@@ -6,8 +6,37 @@
 /*   By: adamiens <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 09:35:56 by adamiens          #+#    #+#             */
-/*   Updated: 2023/02/16 09:36:04 by adamiens         ###   ########.fr       */
+/*   Updated: 2023/02/16 13:57:17 by adamiens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "render.h"
+
+void	free_render_exit(t_texture *config, t_mlx *mlx, char *error)
+{
+	if (config->no != NULL)
+		ft_free((void *)&config->no);
+	if (config->so != NULL)
+		ft_free((void *)&config->so);
+	if (config->we != NULL)
+		ft_free((void *)&config->we);
+	if (config->ea != NULL)
+		ft_free((void *)&config->ea);
+	print_error_exit(error);
+}
+void	render(t_texture *config)
+{
+	t_mlx	mlx;
+
+	mlx.init = mlx_init();
+	if (mlx.init == NULL)
+		free_render_exit(config, &mlx, "Initialisation error\n");
+	mlx.win = mlx_new_window
+		(mlx.init, WIDTH, HEIGHT, "Cub3D");
+	if (mlx.win == NULL)
+		free_render_exit(config, &mlx, "Initialisation error\n");
+	mlx.img = mlx_new_image(mlx.init, WIDTH, HEIGHT);
+	mlx.img_addr = mlx_get_data_addr
+		(mlx.img, &(mlx.bits_per_px),
+			&(mlx.line_len), &(mlx.endian));
+}
